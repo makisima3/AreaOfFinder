93341e19f5e692ed52d40d8850c9d3e6b4b3971d
maks aksenov <maksimya@bk.ru>
2021-03-25 15:57:01 +0300

    intial

---
+++ AreaOfFinder/AreaFinder.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaOfFinder
+{/// <summary>
+/// Напишите на C# библиотеку для поставки внешним клиентам, которая умеет вычислять площадь круга по радиусу и треугольника по трем сторонам. 
+/// Дополнительно к работоспособности оценим:
+/// Юнит-тесты -
+/// Легкость добавления других фигур +
+/// Вычисление площади фигуры без знания типа фигуры +
+/// Проверку на то, является ли треугольник прямоугольным +
+/// </summary>
+    public class AreaFinder
+    {
+        //Учитывая что есть метод "распознающий" фигуру
+        //первые два должны быть private
+        //но для того что бы была возможность вызвать их отдель
+        //они public
+
+        public static (double, string) GetCircleArea(double radius)
+        {
+            return (Math.PI * Math.Pow(radius, 2), "Круг");
+        }
+
+        public static (double, string) GetTriangleArea(double a, double b, double c)
+        {
+            double p = (a + b + c) / 2;
+
+            if (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a)
+            {
+                return (Math.Sqrt(p * (p - a) * (p - b) * (p - c)), "Прямоугольный треугольник");
+            }
+            else
+            {
+                return (Math.Sqrt(p * (p - a) * (p - b) * (p - c)), "Не прямоугольный треугольник");
+            }
+        }
+
+        /// <summary>
+        /// Не совсем понятно как должно быть реализванно распознование
+        /// </summary>
+        /// <returns></returns>
+        public static (double, string) GetUnknownFigureArea(params double[] parameters)
+        {
+            if (parameters.Length == 1)
+            {
+                return (GetCircleArea(parameters[0]));
+            }
+            else if (parameters.Length == 3)
+            {
+                double a = parameters[0], b = parameters[1], c = parameters[2];
+
+                if (a + b > c && a + c > b && b + c > a)
+                {
+                    return GetTriangleArea(a, b, c);
+                }
+                else
+                {
+                    return (-1, "Фигура с 3 сторономи не явзяющаяся треугольником еще не добавлена");
+                }
+            }
+
+            return (-1, "Фигура с таким кол-вом сторон еще не добавлена");
+        }
+    }
+}
---
+++ TestAreaOfFinder/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AreaOfFinder;
+
+namespace TestAreaOfFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region circle
+            Console.WriteLine("Введите раудиус круга:");
+            double r = Convert.ToDouble(Console.ReadLine());
+
+            (double s, string figure) = AreaFinder.GetCircleArea(r);
+            Console.WriteLine($"Площадь = {s}; Фигура: {figure}");
+            #endregion
+
+            #region triangle
+            Console.WriteLine("\nВведите стороны треугольника:");
+
+            Console.WriteLine("Введите a:");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите c:");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите b:");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            (s, figure) = AreaFinder.GetTriangleArea(a, c, b);
+            Console.WriteLine($"Площадь = {s}; Фигура: {figure}");
+            #endregion
+
+            #region unknown
+            Console.WriteLine("\nВведите количество параметров:");
+            int count = Convert.ToInt32(Console.ReadLine());
+
+            double [] figureParams = new double[count];
+            Console.WriteLine("Введите параметры по 1:");
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write(i + ")");
+                figureParams[i] = Convert.ToDouble(Console.ReadLine());
+            }
+
+            (s, figure) = AreaFinder.GetUnknownFigureArea(figureParams);
+            Console.WriteLine($"Площадь = {s}; Фигура: {figure}");
+            #endregion
+            Console.ReadKey();
+        }
+    }
+}
